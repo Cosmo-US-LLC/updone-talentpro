1e8e56558d8eb76d2ff7fac0fbc7d9b03392f700
Abdullah0004439 <ahmadabdullah@cosmoinc.co>
2025-04-17 18:50:06 +0500

    new completed screen color

--- app/_components/booking/worker/components/Completed.tsx
+++ app/_components/booking/worker/components/Completed.tsx
@@ -9,14 +9,14 @@ const Completed = ({ myWorkingDetails, isAssignedToMe, payment_status, jobId }: 
     
     return (
         <div className='lg:max-w-[700px] 2xl:max-w-[1100px] mx-auto flex flex-col mb-[300px]'>
-            <div className="bg-[#FFE9E9] w-full h-[130px] flex items-center border-2 border-[#FFB8B8] lg:px-2 2xl:px-6">
+            <div className="bg-[#ecffe9] w-full h-[130px] flex items-center border-2 border-[#8ffc58] lg:px-2 2xl:px-6">
                 
                 {/* Left Section (Icon + Event Complete Message) */}
                 <div className="flex items-center">
                     <svg width="62" height="72" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M36 66C52.5685 66 66 52.5685 66 36C66 19.4315 52.5685 6 36 6C19.4315 6 6 19.4315 6 36C6 52.5685 19.4315 66 36 66Z" stroke="#C70101" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M36 48V36" stroke="#C70101" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M36 24H36.03" stroke="#C70101" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M36 66C52.5685 66 66 52.5685 66 36C66 19.4315 52.5685 6 36 6C19.4315 6 6 19.4315 6 36C6 52.5685 19.4315 66 36 66Z" stroke="#8ffc58" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M36 48V36" stroke="#8ffc58" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M36 24H36.03" stroke="#8ffc58" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
 
                     {/* Event Message in Column Format */}
